cffe57530bf5b30b5ca5ec971035dc0d8718b740
spriyanshu22 <spriyanshu22@iitk.ac.in>
2024-03-22 12:37:17 +0530

    using sed commands to remove extra spaces

--- bash_scripting.sh
+++ bash_scripting.sh
@@ -12,28 +12,35 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
+input_file=$1
+output_file=$2
+
 # now creating the output file
-touch $2
+touch $output_file
+echo "------------------" >> $output_file
 
 # printing the unique cities from the input file to the output file
-echo "Unique cities in the given data file: " >> $2
-awk -F, 'NR>1{print $3}' $1 | sort | uniq >> $2
-echo "------------------" >> $2
+echo "Unique cities in the given data file: " >> $output_file
+awk -F, 'NR>1{print $3}' $input_file | sort | uniq | sed 's/^[ \t]*//' >> $output_file
+echo "------------------" >> $output_file
 
 # finding the top 3 individuals with the highest salary
-echo "Details of top 3 individuals with the highest salary: " >> $2
-awk -F, 'NR>1{print $4, $0}' $1 | sort -nr | head -n 3 | awk '{print $2, $3, $4, $1}' >> $2
-echo "------------------" >> $2
+echo "Details of top 3 individuals with the highest salary: " >> $output_file
+awk -F, 'NR>1{print $4, $0}' $input_file | sort -nr | head -n 3 | awk '{print $2, $3, $4, $1}' >> $output_file
+echo "------------------" >> $output_file
 
 # computing the average salary for each City in the dataset
-echo "Details of average salary of each city: " >> $2
-awk -F, 'NR>1{sum[$3]+=$4; count[$3]++} END{for(city in sum) print "City:", city, "Salary:", sum[city]/count[city]}' $1 >> $2
-echo "------------------" >> $2
+echo "Details of average salary of each city: " >> $output_file
+awk -F, 'NR>1{sum[$3]+=$4; count[$3]++} END{for(city in sum) print "City:", city, "Salary:", sum[city]/count[city]}' $input_file >> $2
+echo "------------------" >> $output_file
+
+ 
+#Calculate the overall average salary
+avg=$(awk -F, 'NR==1 {next} {s += $4; ct++} END {print s / ct}' "$1")
+# Identify individuals with a salary above the overall average salary
+echo "Details of individuals with a salary above the overall average salary: " >> $output_file
+awk -F, -v avg="$avg" 'NR>1 && $4 > avg {print $1, $2, $3, $4}' $input_file | sed 's/above/over/' >> $output_file
+echo "------------------" >> $output_file
 
 
-# printing the details of individuals with a salary above the overall average salary
-echo "Details of individuals with a salary above the overall average salary: " >> $2
-awk -F, 'NR>1{sum+=$4; count++} END{avg=sum/count} {if($4>avg) print $1, $2, $3, $4}' $1 >> $2
-echo "------------------" >> $2
-
-echo "Output has been saved to $2a"
+echo "Output has been saved to $output_file."
